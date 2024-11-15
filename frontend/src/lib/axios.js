74aa75d250f669da244d80cacac200097b98b0c7
oduncica <oduncica@gmail.com>
2024-11-15 09:53:27 +0100

    feat(frontend/auth): implement and test user authentication, login, and logout
    - Added user signup functionality with genre preferences
    - Implemented user login and JWT token handling
    - Added logout functionality to clear JWT token
    - Integrated authentication state management with Zustand
    - Tested all features on the frontend and confirmed working

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,17 @@
- import axios from 'axios';
+import axios from 'axios';
 
- export const axiosInstance = axios.create({
-      baseURL: 'http://localhost:3000/api',
-      withCredentials: true,
-  });
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  withCredentials: true, // Assurez-vous que les cookies sont envoyés avec chaque requête
+});
+
+// Ajouter un intercepteur pour inclure le token JWT dans les en-têtes des requêtes
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwt');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
