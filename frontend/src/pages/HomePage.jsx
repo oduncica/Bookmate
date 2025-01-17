3e2283857d5fd8f0423f05a5c8f6685f0279b1f1
oduncica <oduncica@gmail.com>
2025-01-17 12:11:31 +0100

    update react-swipeable and react-modal

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSuggestionsStore } from '../store/useSuggestionsStore'; // Utilisez le nouveau store
-import TinderBookCard from '../components/TinderBookCard';
-import { useSwipeable } from 'react-swipeable';
+import React, { useEffect, useState } from "react";
+import { useSuggestionsStore } from "../store/useSuggestionsStore"; // Utilisez le nouveau store
+import TinderBookCard from "../components/TinderBookCard";
+import { useSwipeable } from "react-swipeable";
 
 const HomePage = () => {
-  const { suggestions, fetchSuggestions, likeBook, dislikeBook, readBook } = useSuggestionsStore();
+  const { suggestions, fetchSuggestions, likeBook, dislikeBook, readBook } =
+    useSuggestionsStore();
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -12,36 +13,47 @@ const HomePage = () => {
   }, [fetchSuggestions]);
 
   const handleSwipe = (direction) => {
-    if (direction === 'right' && currentIndex > 0) {
+    if (direction === "right" && currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
-    } else if (direction === 'left' && currentIndex < suggestions.length - 1) {
+    } else if (direction === "left" && currentIndex < suggestions.length - 1) {
       setCurrentIndex(currentIndex + 1);
     }
   };
 
   const handlers = useSwipeable({
-    onSwipedLeft: () => handleSwipe('left'),
-    onSwipedRight: () => handleSwipe('right'),
+    onSwipedLeft: () => handleSwipe("left"),
+    onSwipedRight: () => handleSwipe("right"),
   });
 
   const handleLike = async (bookId) => {
     await likeBook(bookId);
-    setCurrentIndex((prevIndex) => (prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex));
+    setCurrentIndex((prevIndex) =>
+      prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex
+    );
   };
 
   const handleDislike = async (bookId) => {
     await dislikeBook(bookId);
-    setCurrentIndex((prevIndex) => (prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex));
+    setCurrentIndex((prevIndex) =>
+      prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex
+    );
   };
 
   const handleRead = async (bookId) => {
     await readBook(bookId);
-    setCurrentIndex((prevIndex) => (prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex));
+    setCurrentIndex((prevIndex) =>
+      prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex
+    );
   };
 
   return (
-    <div className="container mx-auto px-4 py-8 bg-[#3A3A64] min-h-screen flex flex-col items-center justify-center" {...handlers}>
-      <h1 className="text-4xl font-bold text-center mb-8 text-white">BookMate</h1>
+    <div
+      className="container mx-auto px-4 py-8 bg-[#3A3A64] min-h-screen flex flex-col items-center justify-center"
+      {...handlers}
+    >
+      <h1 className="text-4xl font-bold text-center mb-8 text-white">
+        BookMate
+      </h1>
       <div className="flex justify-center items-center w-full h-full">
         {Array.isArray(suggestions) && suggestions.length > 0 ? (
           <TinderBookCard
@@ -51,7 +63,9 @@ const HomePage = () => {
             onRead={handleRead}
           />
         ) : (
-          <p className="text-center text-xl text-white">Aucune suggestion de livre disponible.</p>
+          <p className="text-center text-xl text-white">
+            Aucune suggestion de livre disponible.
+          </p>
         )}
       </div>
     </div>
