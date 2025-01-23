a3167772c4c0f0a9e8e6bf40d7bbe39018090a97
oduncica <oduncica@gmail.com>
2025-01-23 21:18:21 +0100

    update git ignore files

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import matchRoutes from "./routes/matchRoutes.js";
 import libraryRoutes from "./routes/libraryRoutes.js";
 import { connectDB } from "./config/db.js";
-import path from "path";
+
 dotenv.config();
 
 const app = express();
@@ -23,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173" || "https://bookmate-gh7l.onrender.com/",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -34,10 +35,10 @@ app.use("/api/match", matchRoutes);
 app.use("/api/library", libraryRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
   withCredentials: true, // Assurez-vous que les cookies sont envoyés avec chaque requête
 });
 
