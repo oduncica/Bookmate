c3ee7befea9297114a45cce72197c26423d7fef1
oduncica <oduncica@gmail.com>
2025-01-23 20:23:39 +0100

    update build

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import matchRoutes from "./routes/matchRoutes.js";
 import libraryRoutes from "./routes/libraryRoutes.js";
 import { connectDB } from "./config/db.js";
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const __dirname = path.resolve();
 
 // Connexion à la base de données
 connectDB();
@@ -24,10 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://bookmate-gh7l.onrender.com/"
-        : "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://bookmate-gh7l.onrender.com/",
     credentials: true,
   })
 );
@@ -37,13 +32,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/match", matchRoutes);
 app.use("/api/library", libraryRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
-
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "http://localhost:3000/api",
   withCredentials: true, // Assurez-vous que les cookies sont envoyés avec chaque requête
 });
 
